776c5abd7c05b668faba85d85c407713be522db9
George Kimari <grgkimari@gmail.com>
2023-02-14 00:03:33 +0300

    passed tests

--- index.js
+++ index.js
@@ -1,60 +1,89 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const dns = require('node:dns') 
-const bodyParser = require('body-parser')
+const dns = require("node:dns");
+const bodyParser = require("body-parser");
+const open = require("open");
+const URL = require('url').URL
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : false}))
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
+
+//Links
+
+let linkStore = []
+
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-let linkKeys = {}
 
 app.post('/api/shorturl', (req, res) => {
-  let uneditedURL =  req.body.url
-  let url = uneditedURL
-  url = url.replace(/^https?:\/\//,'')
-  url = url.slice(0,url.indexOf('/'))
-
-  console.log( "URL : " + url)
-  
-  dns.lookup(url, (err, addresses,family) => {
-    console.log(err)
-      console.log(addresses)
-      console.log(family)
-    if(err !== null){
-      
-      res.json({
-        error : 'invalid url'
-      })
-     
-    }
-else{
-  linkKeys[Math.floor(Math.random() * 10009)] = uneditedURL
-
-    res.json({
-      original_url : uneditedURL,
-      short_url : linkKeys[uneditedURL] ?  linkKeys[uneditedURL] : "Does not exist" 
-    })
+let invalidObj = {
+  "error" : "invalid url"
 }
-    
+
+  let originalURL = req.body.url
+try{
+  new URL(originalURL)
+  }
+  catch(err) {
+    return res.json(invalidObj)
+  }
+
+  console.log("original url : " + originalURL)
+  let editedURL = originalURL.replace(/^(https?|ftp):\/\/?/,'')
+  editedURL= editedURL.slice(0, editedURL.indexOf('/'))
+  console.log("edited url : " + editedURL)
+
+  dns.lookup(editedURL, (err) => {
+    if(err){
+      console.log("Invalid domain")
+      return res.json(invalidObj)
+    }
+    else{
+      let obj
+      if(linkStore.indexOf(originalURL) === -1){
+        linkStore.push(originalURL)
+        
+      }
+      obj = {
+        "original_url" : `${originalURL}`,
+        "short_url" : `${linkStore.indexOf(originalURL)}`
+      }
+      console.log("Returning : " + JSON.stringify(obj))
+      return res.json(obj)
+    }
   })
+
+
 })
 
-app.get('api/shorturl/:url')
+app.get('/api/shorturl/:index', (req, res) => {
+  let index = req.params.index
 
-app.listen(port, function() {
+  if(linkStore[index]){
+    return res.redirect(linkStore[index])
+}
+else{
+  res.json({
+    error : "short url not found"
+  })
+}
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
